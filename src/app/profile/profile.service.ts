d237ec34b2b9d778ad1028503da4c0179385c023
mehulpatilui <patilmehul1998@gmail.com>
2023-04-01 17:49:36 +0530

    initial commit

---
+++ src/app/profile/profile.service.ts
@@ -0,0 +1,379 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProfileService {
+  //baseUrl = environment.baseUrl;
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  projects:any = [
+    {
+      id: 1,
+      title: 'Travel booking proccess site',
+      desc: 'Make your business shine online with a custom travel website developed just for you by a professional developer.',
+      livedemo: 'https://mehulk05.github.io/MyShop/#/',
+      githurl: 'https://github.com/mehulk05/MyShop',
+      mediumlink: 'https://itnext.io/read-data-from-google-spreadsheet-in-angular-209ee74b6cb4',
+      imgUrl: 'assets/images/Travelling.png',
+      tech: 'Angular 10 , Google SpreadsheetApi'
+    },
+
+    {
+      id: 2,
+      title: 'Online drink selling site',
+      desc: 'If you have the guts to earn money from Fruit juice then it’s a very good business opportunity to earn handsome profits.',
+      livedemo: 'https://mehulk05.github.io/Blogapp-using-MERN/#/',
+      githurl: 'https://github.com/mehulk05/Blogapp-using-MERN',
+      mediumlink: 'https://itnext.io/blog-app-using-mern-stack-b0b4d69d7ea1',
+      imgUrl: 'assets/images/Shope.png',
+      tech: 'Angular 9, Node, MongoDB, Express'
+    },
+
+
+    {
+      id: 3,
+      title: 'Online business grow webapp',
+      desc: 'Choose a business idea that you are knowledgeable and passionate about, and develop a detailed business plan.',
+      livedemo: 'https://mehulk05.github.io/Blog-using-mean/#/',
+      githurl: 'https://github.com/mehulk05/Blog-using-mean',
+      mediumlink: 'https://itnext.io/crud-operation-using-mean-stack-7dfa2f51ec8c',
+      imgUrl: 'assets/images/Business_idea.png',
+      tech: 'React JS, Node, MongoDB, Express'
+    },
+
+    {
+      id: 4,
+      title: 'Business finance analise platform.',
+      desc: 'Comparison Slides - creative design business presentation templates in the Keynote. Ready template, easy to use.',
+      livedemo: 'https://chatapp-using-node.herokuapp.com/',
+      githurl: 'https://github.com/mehulk05/Chat-app-using-Nodejs',
+      mediumlink: 'https://medium.com/weekly-webtips/building-a-multiroom-chat-application-in-node-js-8a8adca5acf2',
+      imgUrl: 'assets/images/Admition_application.png',
+      tech: 'Electron JS, sNode JS, Socket IO'
+    },
+
+
+    {
+      id: 5,
+      title: 'Online platform to find your home',
+      desc: 'Whether buying or renting, finding the right place to live is a personal and unique experience.',
+      livedemo: 'https://mehulk05.github.io/Medium-Blog-On-React-App#/',
+      githurl: 'https://github.com/mehulk05/Medium-Blog-On-React-App',
+      mediumlink: 'https://javascript.plainenglish.io/how-to-add-medium-blogs-on-your-react-app-a3898a729aad',
+      imgUrl: 'assets/images/find_home.png',
+      tech: 'React JS, node JS, SQLS'
+    },
+
+
+
+    {
+      id: 6,
+      title: 'International travel booking webapp',
+      desc: 'No one realizes how beautiful it is to travel until he comes home and rests his head on his old, familiar pillow.',
+      livedemo: 'https://mehulk05.github.io/NewsApp-using-React',
+      githurl: 'https://github.com/mehulk05/NewsApp-using-React',
+      mediumlink: 'https://codeburst.io/news-app-using-reactjs-f9752d7ab6ce',
+      imgUrl: 'assets/images/travel_mobile.png',
+      tech: 'Angular 11, XMl to JSON'
+    },
+
+
+
+    // {
+    //   id: 7,
+    //   title: 'Medium-Like Blog App Using Angular 9 and Firebase',
+    //   desc: '',
+    //   livedemo: 'https://mehulk05.github.io/Blog-Book/',
+    //   githurl: 'https://github.com/mehulk05/Blog-Book',
+    //   mediumlink: 'https://codeburst.io/blogging-app-using-angular-9-and-firebase-98e36168d850',
+    //   imgUrl: 'assets/images/z21.png',
+    //   tech: 'HTML, Bootstrap, CSS, Angular-9, Firebase'
+    // },
+
+
+
+    // {
+    //   id: 8,
+    //   title: 'COVID 19 Tracker (Statistics) app Using Angularr',
+    //   desc: '',
+    //   livedemo: 'https://mehulk05.github.io/COVOID-19/',
+    //   githurl: 'https://github.com/mehulk05/COVOID-19',
+    //   mediumlink: 'https://codeburst.io/covoid-19-tracker-statistics-app-using-angular-2358347b3d1f',
+    //   imgUrl: 'assets/images/z2.png',
+    //   tech: 'Angular 10 ,Bootstrap,Javascript'
+    // },
+
+
+    // {
+    //   id: 9,
+    //   title: 'Weather App with React',
+    //   desc: '',
+    //   livedemo: 'https://mehulk05.github.io/React-Weather/',
+    //   githurl: 'https://github.com/mehulk05/React-Weather-App',
+    //   mediumlink: 'https://medium.com/analytics-vidhya/weather-app-with-react-adaa7268bbdb',
+    //   imgUrl: 'assets/images/z9.png',
+    //   tech: 'React, Open MAp Api'
+    // },
+
+
+    // {
+    //   id: 10,
+    //   title: 'Spring Boot + Angular 8 CRUD Example',
+    //   desc: '',
+    //   livedemo: 'https://github.com/mehulk05/Spring-Boot-Angular-8-CRUD-Example',
+    //   githurl: 'https://github.com/mehulk05/Spring-Boot-Angular-8-CRUD-Example',
+    //   mediumlink: 'https://medium.com/analytics-vidhya/spring-boot-angular-8-crud-example-8aeafd47b54',
+    //   imgUrl: 'assets/images/z28.png',
+    //   tech: 'Angular8, Java ,Hibernate'
+    // },
+
+
+    // {
+    //   id: 11,
+    //   title: 'Personal Portfolio Using Angular',
+    //   desc: '',
+    //   livedemo: 'https://personal-portfolio0320.000webhostapp.com/',
+    //   githurl: 'https://github.com/mehulk05/Personal-Portfolio-Angular-Updated',
+    //   mediumlink: 'https://javascript.plainenglish.io/personal-portfolio-using-angular-8-and-bootstrap-5dde6fc7cf9b',
+    //   imgUrl: 'assets/images/z11.png',
+    //   tech: 'Angular ,Bootstrap'
+    // },
+
+
+    // {
+    //   id: 12,
+    //   title: 'Personal Portfolio Using Django',
+    //   desc: '',
+    //   livedemo: 'https://personal-portfolio0320.000webhostapp.com/',
+    //   githurl: 'https://github.com/mehulk05/My-Personal-Portfolio',
+    //   mediumlink: 'https://mehulkothari05.medium.com/personal-portfolio-using-django-dba49c355905',
+    //   imgUrl: 'assets/images/z12.png',
+    //   tech: 'Django'
+    // },
+
+    // {
+    //   id: 13,
+    //   title: 'Movie Success Prediction',
+    //   desc: '',
+    //   livedemo: 'https://mehulkothari05.medium.com/',
+    //   githurl: 'https://github.com/mehulk05',
+    //   mediumlink: 'https://mehulkothari05.medium.com/movie-success-prediction-a67d871a7b1',
+    //   imgUrl: 'assets/images/z13.png',
+    //   tech: 'Python, Machine Learning, Django Framework.'
+    // },
+
+    // {
+    //   id: 14,
+    //   title: 'Matrimonial Website',
+    //   desc: '',
+    //   livedemo: 'https://mehulkothari05.medium.com/',
+    //   githurl: 'https://github.com/mehulk05/Matrimonial-Website.git',
+    //   mediumlink: 'https://mehulkothari05.medium.com/matrimonial-website-9cd5247f477d',
+    //   imgUrl: 'assets/images/z14.png',
+    //   tech: 'HTML, CSS, Bootstrap, Jquery'
+    // },
+
+  ]
+
+  about2 = [{ p: 'Software Developer with 1.4+ years of experience.'},
+  { p: 'Worked with Software Development Life Cycle(SDLC) and WebRTC.'},
+  { p: 'Expertise in Angular 1.6+ All versions, Javascript.' },
+  { p: 'Have good understanding of WebRTc protocol,FCM Cloud messaging and multilingual.'}]
+
+  about = "I'm a native of Indore, multi-faceted individual with a Master's degree in Computer Applications and currently working as a professional Software engineer. With a strong foundation in computer science, I've a keen interest in traveling and exploring new cultures. my passion for adventure and the great outdoors is evident in i love for travel, as i seeks out new experiences and delves into the unfamiliar. With a thirst for knowledge and an insatiable curiosity, I'm always eager to learn and grow, whether it be through my professional pursuits or personal experiences."
+  resumeurl = "https://docs.google.com/document/d/15RWZxsIv-l-0YOq3iMPjtkHVDMVYYI6l/edit?rtpof=true"
+
+  skillsData: any = [
+    {
+      'id': '1',
+      'skill': 'ANGULAR 8+',
+      'progress': '85%'
+    },
+    {
+      'id': '2',
+      'skill': 'JAVASCRIPT',
+      'progress': '80%'
+    },
+    {
+      'id': '3',
+      'skill': 'Typescript',
+      'progress': '85%'
+    },
+    {
+      'id': '4',
+      'skill': 'CSS, SCSS',
+      'progress': '55%'
+    },
+    {
+      'id': '5',
+      'skill': 'Material Design(Angular Material)',
+      'progress': '90%'
+    },
+    {
+      'id': '6',
+      'skill': 'NGX Bootstrap',
+      'progress': '90%'
+    },
+  ];
+
+
+  educationData: any = [
+    {
+      'id': '1',
+      'from_to_year': '2019 - 2021',
+      'education': 'Master\'s Degree',
+      'stream': 'Master of Computer Application',
+      'info': `Aditya Silver Oak Institute is GTU Affiliated and ranks 7th in Gujurat
+Completed B.E in Computer Engineering with 9.54 CGPA.
+Won the Best Student Award 3 times for excellent Academic records at College.
+Maintained above 9.1 spi in every semester of college academic .`,
+      'institution': 'ADITYA SILVER OAK INSTITUTE OF TECHNOLOGY, AHMEDABAD'
+    },
+    {
+      'id': '2',
+      'from_to_year': '2015 - 2019',
+      'education': 'Bachelor\'s Degree',
+      'stream': 'Bachelor of Engineering',
+      'info': `Aditya Silver Oak Institute is GTU Affiliated and ranks 7th in Gujurat
+Completed B.E in Computer Engineering with 9.54 CGPA.
+Won the Best Student Award 3 times for excellent Academic records at College.
+Maintained above 9.1 spi in every semester of college academic .`,
+      'institution': 'ADITYA SILVER OAK INSTITUTE OF TECHNOLOGY, AHMEDABAD'
+    },
+    {
+      'id': '3',
+      'from_to_year': '2013 - 2015',
+      'education': 'Higher Secondary',
+      'stream': 'Science and Mathematics',
+      'institution': 'GYANDEEEP VIDHYALAYA,AHMEDABAD',
+      'info': `The High School was mainly focus on the bases of science, mathematics, and Chemistry
+There was also a good base on physics, mathematics and chemistry.
+Completed my high school with 78%.`
+    },
+    {
+      'id': '4',
+      'from_to_year': '2012 - 2013',
+      'education': 'Secondary  School',
+      'stream': 'Science and Mathematics',
+      'institution': 'GYANDEEEP VIDHYALAYA,AHMEDABAD',
+      'info': `The Secondary aims at Maths , English , Science, Social Science, Litrature and Languages.
+Completed my Secondary school with 84%.`
+    }
+  ];
+
+  exprienceData: any = [
+    {
+      id: 4,
+      company: 'Moontechnolabs PVT. LTD',
+      location: 'Ahmedabad',
+      timeline: 'Jan 2022 to Present',
+      role: 'Angular Developer',
+      work: `Technologies : Angular , JavaScript , jQuery  .
+
+  Project 1 : Working on WebRTC WebApplication. Manage to call flow and port number Identifying areas for modification in the current project to implement. Identifying areas for modification in the current project to implement.
+  . Identifying areas for modification in the current project to implement. .
+
+  Project 2 : Manage payment integration using stripe payment gateway. Chatbot integration(Trengo chat). implement multilingual (English and French).  .
+
+  Working closely with developers, UI designers, business developers, and quality analytics.
+  `
+    },
+    {
+      id: 3,
+      company: 'Shahsoftsol.',
+      location: 'Indore',
+      timeline: 'Mar 2021 to May 2021',
+      role: 'UI/UX Developer',
+      work: `Technologies : Angular , JavaScript , Bootstrap . 
+      Worked on an e-commerce web application that facilitated online transactions of goods through means of transfer of information and funds over the internet.  `
+    },
+  //   {
+  //     id: 2,
+  //     company: 'Growth99',
+  //     location: 'Remote',
+  //     timeline: '',
+  //     role: 'Frontend  Developer',
+  //     work: `Technologies : Angular , Node JS , MongoDB .
+  // Worked on the product of Growth99 which allowed other clients to come and integrate our product in their website .
+  // Worked on REST Api , Admin Dashboard , Appointment Booking for services and UI of the application .
+  // Created sections like templates for email, sms, landing pages, clinic, services, providers and triggers Automation.
+  // `
+  //   },
+
+  //   {
+  //     id: 1,
+  //     company: 'Tata Consultancy Services',
+  //     location: 'Gandhinagar and Pune',
+  //     timeline: 'June 2019 - Jan 2021',
+  //     role: 'Assistant System Engineer Trainee',
+  //     work: `Technologies : Angular , JavaSpring Boot , SQL. Apache Solr .
+
+  // Project 1 : Worked on uploading two excel sheets with bulk of data and finding the matches for each business .
+
+  // Project 2 : Worked on indexing the search results inside the application with bulk of data using Apache Solr and configuring the server with cron jobs and shell scripts .
+
+  // Worked on ranking the search results based on various criteria,features and delivering the accurate results based on generated scores.
+  // `
+  //   },
+
+
+  ]
+
+  extraCircularInfo: any = [
+    {
+      id:1,
+      title: 'Blogger',
+      description: '',
+      img: 'assets/images/Mehul Kothari – Medium1.png',
+      url: 'https://mehulkothari05.medium.com/'
+    },
+    {
+      id:1,
+      title: 'Github',
+      description: '',
+      img: 'assets/images/Mehulkothari_github.png',
+      url: 'https://github.com/mehulk05/'
+    },
+    {
+      id:1,
+      title: 'Freelancing',
+      description: '',
+      img: 'assets/images/mehul_freelancing.png',
+      url: 'https://www.upwork.com/freelancers/~016d6f497a5629df4b'
+    },
+  ]
+  // contactus(data: any): Observable<any> {
+  //   return this.http.post(this.baseUrl + 'contact', data);
+  // }
+
+  skills(): Observable<any> {
+    // return this.http.get(this.baseUrl + 'skills');
+    return this.skillsData;
+  }
+
+  getProjects(): Observable<any> {
+    // return this.http.get(this.baseUrl + 'skills');
+    return this.projects;
+  }
+  education(): Observable<any> {
+    // return this.http.get(this.baseUrl + 'education');
+    return this.educationData;
+  }
+
+  exprience(): Observable<any> {
+    // return this.http.get(this.baseUrl + 'exprience');
+    return this.exprienceData;
+  }
+
+  extraCircular(): Observable<any> {
+    // return this.http.get(this.baseUrl + 'exprience');
+    return this.extraCircularInfo;
+  }
+}
