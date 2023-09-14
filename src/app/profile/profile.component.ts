462f34b83146ab59990e39abba117eaa6b4b9731
mehulpatilui <patilmehul1998@gmail.com>
2023-09-14 23:40:38 +0530

    add cookie card

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -6,7 +6,7 @@ import { MatDialog } from '@angular/material/dialog';
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
-  isOpencard: boolean = false;
+  isOpencard: boolean = true;
   cookiesValue = 'Wt8tV4MEfZab9tjJacrn41mEZw3JGmbWA6FlWeTbXTgR5s9JrDushueSk8RAYgrubijWVUowRyelC8fH8MktMN8i5fuQazaQ7lMoJN1iTb6v0lkIRhMantaVoGc8uLDjRO9j6FdXB7vAGymQ9mhtnnGLEac3EVn347AyOQCsmhcyl2I5FatH4G0Mijvma4Cf2Ioigqjra132awoFd6AX3rE1nGKGq0wEOSkcDWxpThRrcBC2pOBsDxwcfva756Jwe5aJv13bmw5svE5nVNK0aFvjy42AJS16UH5duJCzFvzL'
   constructor(private dialog: MatDialog) { }
 
